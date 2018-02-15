8b9b04d003d5d34229b1eb78c47a4773c2bce2da
gcandrade10 <gc.andrade10@uniandes.edu.co>
2018-02-15 09:10:00 -0500

    fixed navbar

--- scripts/actualizarModal.js
+++ scripts/actualizarModal.js
@@ -2,30 +2,30 @@
 // Get the modal
 var modal = document.getElementById('myModal');
 var imgs = document.getElementsByClassName("modals");
-for (var i = imgs.length - 1; i >= 0; i--) {
+for (var i = imgs.length - 1; i >= 0; i--) 
+{
 	asignar(imgs[i]);
 }
-
 function asignar(img)
 {
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-img.onclick = function(){
-    var b=document.getElementById("boton");
-console.log(img);
-var route = img.src.split("images")[1].split(".")[0];
-route="./jsons"+route+".json";
-//route=route.replace("/","\\");
-console.log(route);
-var json=$.getJSON(route,function(data){
-	console.log(data);
-$("#titulo").text(data.titulo);
-$("#descripcion").text(data.descripción);
-$("#imagen").attr("src",data.imagen);
-$("#position").text("Position: "+data.position);
-$("#date").text("Date: "+data.date);
-$("#link").attr("href",data.link);
-});
-    b.click();
-}
+	img.onclick = function()
+	{
+		var b=document.getElementById("boton");
+		console.log(img);
+		var route = img.src.split("images")[1].split(".")[0];
+		route="./jsons"+route+".json";
+		//route=route.replace("/","\\");
+		console.log(route);
+		var json=$.getJSON(route,function(data)
+		{
+			console.log(data);
+			$("#titulo").text(data.titulo);
+			$("#descripcion").text(data.descripción);
+			$("#imagen").attr("src",data.imagen);
+			$("#position").text("Position: "+data.position);
+			$("#date").text("Date: "+data.date);
+			$("#link").attr("href",data.link);
+		});
+		    b.click();
+	}
 }
